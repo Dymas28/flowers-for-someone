6620ceacaf9f008d53a91990ca7614e28af4bd51
Dymas28 <dymasferdyas@gmail.com>
2025-08-19 23:28:29 +0700

    main.js
    roa

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('SAYAAANGGG PUNDUNGGG').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
